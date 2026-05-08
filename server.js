c793b55f8f4d44e0f075d5c0c3015cd08d5887b3
Rakshit Panwar <rakshitpanwar03@gmail.com>
2026-05-08 23:35:47 +0530

    coomit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173", // Adjust as needed for your frontend
   credentials: true, // Allow cookies to be sent
-C}));
+}));
 
 // Add request logging middleware BEFORE routes
 app.use(requestLogger);
